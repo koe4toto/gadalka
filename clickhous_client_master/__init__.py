8d4dc99cfc4061f8fb0d7c631c6b3388edbe6b25
koe4toto <37112551+koe4toto@users.noreply.github.com>
2019-03-13 14:55:21 +0300

    -

--- clickhous_client_master/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'yurial <clickhouse@yurial.ru>'
-
-from clickhous_client_master.client import ClickHouseClient
-
-all = ['ClickHouseClient']
--- prototype.py
+++ prototype.py
@@ -13,18 +13,3 @@ import databases.db_app as db_app
 import databases.db_data as db_data
 import databases.db_queue as db_queue
 from itertools import groupby
-from clickhous_client_master.client import ClickHouseClient
-from clickhous_client_master.errors import Error as ClickHouseError
-
-def on_progress(total, read, progress):
-    print(total,read,progress)
-
-try:
-    client = ClickHouseClient('clickhouse://default:lifeisgood@10.16.16.161:8123/default', on_progress=on_progress, user='api', password='apipass')
-    query = 'SELECT date FROM adfox.dist_elog WHERE date > toDate(0)'
-    result = client.select(query, send_progress_in_http_headers=1)
-    print(result.data)
-except ClickHouseError as e:
-    print(e)
-except Exception as e:
-    print(e)
