c55ad0dde6df58b1fd3b1242a7392ec5e7bdf9a5
koe4toto <37112551+koe4toto@users.noreply.github.com>
2023-01-03 15:30:21 +0300

    react

---
+++ react/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <>
+        <h1>Привет</h1>
+        <p className="h1">Тест</p>
+        <img />
+      </>
+  );
+}
+
+export default App;
