27c69ceb9c7df75fc38beecfe5cdb76c0b804241
koe4toto <37112551+koe4toto@users.noreply.github.com>
2023-01-03 16:45:17 +0300

    отладка

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
       <>
-        <h1>Привет</h1>
-        <p className="h1">Тест</p>
-        <img />
+          <h1>Привет!</h1>
+          <p className="h1">Тест</p>
+          <img src="/images/logo192.png" alt="Картинка" title="Картиночка" />
+          <p alt="">Тест</p>
       </>
   );
 }
 
-export default App;
+export default App; 
