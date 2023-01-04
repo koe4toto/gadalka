61e9c9074c8ad6ff8a024e8c24973fcd64df6350
koe4toto <37112551+koe4toto@users.noreply.github.com>
2023-01-04 14:51:15 +0300

    MIUI

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -5,8 +5,13 @@ function App() {
       <>
           <h1>Привет!</h1>
           <p className="h1">Тест</p>
+          <select>
+              <option value="1">one</option>
+              <option value="2">two</option>
+          </select>
           <img src="/images/logo192.png" alt="Картинка" title="Картиночка" />
           <p alt="">Тест</p>
+          
       </>
   );
 }
